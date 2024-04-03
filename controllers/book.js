56713560894792e662d9119e60ddb9f89c8f9465
pusapativarsha <pusapativarsha26@gmail.com>
2024-04-03 17:06:48 -0500

    commit

--- controllers/book.js
+++ controllers/book.js
@@ -54,7 +54,7 @@ exports.book_create_post = async function(req, res) {
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
     // {"book_type":"goat", "cost":12, "size":"large"}
-    document.book_tile = req.body.book_title;
+    document.book_title = req.body.book_title;
     document.book_author = req.body.book_author;
     document.book_price = req.body.book_price;
     try{
@@ -67,5 +67,25 @@ exports.book_create_post = async function(req, res) {
     }
     };
    
-   
+   // Handle book create on POST.
+exports.book_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new book();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"book_type":"goat", "cost":12, "size":"large"}
+    document.book_title = req.body.book_title;
+    document.book_author = req.body.book_author;
+    document.book_price = req.body.book_price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
  
