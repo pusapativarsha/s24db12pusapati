feaf1c17af925425ee8668a67dd0f2d8c536de85
pusapativarsha <pusapativarsha26@gmail.com>
2024-04-10 13:02:25 -0500

    Update book.js

--- controllers/book.js
+++ controllers/book.js
@@ -5,16 +5,16 @@ exports.book_list = function(req, res) {
 };
 // for a specific book.
 exports.book_detail = async function(req, res) {
-    console.log("detail" + req.params.id)
-    try {
-    result = await book.findById( req.params.id)
-    res.send(result)
-    } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
-    }
-    };
-    
+console.log("detail" + req.params.id)
+try {
+result = await book.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
 // Handle book create on POST.
 exports.book_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: book create POST');
@@ -23,7 +23,6 @@ exports.book_create_post = function(req, res) {
 exports.book_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: book delete DELETE ' + req.params.id);
 };
- 
 // Handle book update form on PUT.
 exports.book_update_put = async function(req, res) {
 console.log(`update on id ${req.params.id} with body
@@ -33,8 +32,8 @@ let toUpdate = await book.findById( req.params.id)
 // Do updates of properties
 if(req.body.book_title)
 toUpdate.book_title = req.body.book_title;
-if(req.body.author) toUpdate.author = req.body.author;
-if(req.body.price) toUpdate.price = req.body.price;
+if(req.body.book_author) toUpdate.book_author = req.body.book_author;
+if(req.body.book_price) toUpdate.book_price = req.body.book_price;
 let result = await toUpdate.save();
 console.log("Sucess " + result)
 res.send(result)
@@ -46,8 +45,6 @@ failed`);
 };
 
 
- 
- 
 exports.book_list = async function(req, res) {
     try{
     thebook = await book.find();
@@ -70,39 +67,17 @@ exports.book_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
- 
- 
-// Handle book create on POST.
+
+
+    // Handle book create on POST.
 exports.book_create_post = async function(req, res) {
     console.log(req.body)
     let document = new book();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"book_type":"goat", "cost":12, "size":"large"}
-    document.book_title = req.body.book_title;
-    document.book_author = req.body.book_author;
-    document.book_price = req.body.book_price;
-    try{
-    let result = await document.save();
-    res.send(result);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
-   
-   // Handle book create on POST.
-exports.book_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new book();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"book_type":"goat", "cost":12, "size":"large"}
-    document.book_title = req.body.book_title;
-    document.book_author = req.body.book_author;
+    document.book_type = req.body.book_type;
+    document.book_size = req.body.book_size;
     document.book_price = req.body.book_price;
     try{
     let result = await document.save();
@@ -114,4 +89,3 @@ exports.book_create_post = async function(req, res) {
     }
     };
     
- 
